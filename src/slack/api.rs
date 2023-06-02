3949b5be81a48218ec114d7e0f0aa8b6584af40d
Sam A. Horvath-Hunt <hello@samhh.com>
2023-06-02 11:42:27 +0100

    Reuse client instance
    This'll be more relevant once we have a long-running server making these calls.

--- src/slack/api.rs
+++ src/slack/api.rs
@@ -1,13 +1,18 @@
 use super::auth::TOKEN;
 use crate::error::Failure;
-use reqwest;
+use once_cell::sync::Lazy;
 
 const API_BASE: &str = "https://slack.com/api";
+
+/// A reusable client that holds a connection pool internally, as per
+/// <reqwest::Client>. `Lazy` allows us to safely reuse the client within this
+/// module rather than drill it all the way down from the router.
+static CLIENT: Lazy<reqwest::Client> = Lazy::new(reqwest::Client::new);
 
 /// Create a GET request to the specified `path` endpoint, handling
 /// authentication.
 pub fn get<T: ToString>(path: T) -> reqwest::RequestBuilder {
-    reqwest::Client::new()
+    CLIENT
         .get(API_BASE.to_owned() + &path.to_string())
         .header(reqwest::header::AUTHORIZATION, "Bearer ".to_owned() + TOKEN)
 }
@@ -15,7 +20,7 @@ pub fn get<T: ToString>(path: T) -> reqwest::RequestBuilder {
 /// Create a POST request to the specified `path` endpoint, handling
 /// authentication.
 pub fn post<T: ToString>(path: T) -> reqwest::RequestBuilder {
-    reqwest::Client::new()
+    CLIENT
         .post(API_BASE.to_owned() + &path.to_string())
         .header(reqwest::header::AUTHORIZATION, "Bearer ".to_owned() + TOKEN)
 }
