7fc3194b33fb3c2c4b556930b522a15dd66bb4f8
Sam A. Horvath-Hunt <hello@samhh.com>
2023-06-06 12:21:09 +0100

    Add E2E test for health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use router::Deps;
 use slack::api::{SlackClient, API_BASE};
 use std::net::SocketAddr;
 use std::sync::Arc;
-use tokio::sync::Mutex;
+use tokio::sync::{oneshot, Mutex};
 use tracing::{info, warn};
 
 mod de;
@@ -35,6 +35,18 @@ async fn main() {
         .unwrap_or(80);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
+
+    server_(addr).await;
+}
+
+/// Initialise a server without graceful shutdown.
+async fn server_(addr: SocketAddr) {
+    // Giving a receiver that will never resolve.
+    server(addr, oneshot::channel::<()>().1).await;
+}
+
+/// Initialise a server with graceful shutdown via `rx`.
+async fn server(addr: SocketAddr, rx: oneshot::Receiver<()>) {
     info!("Listening on {}", addr.to_string());
 
     let slack_client = SlackClient::new(API_BASE.into());
@@ -44,6 +56,40 @@ async fn main() {
 
     axum::Server::bind(&addr)
         .serve(router::new(deps).into_make_service())
+        .with_graceful_shutdown(async {
+            rx.await.ok();
+        })
         .await
         .expect("Failed to start server");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::http::StatusCode;
+
+    #[tokio::test]
+    async fn test_real_health_api() {
+        let (tx, rx) = oneshot::channel::<()>();
+
+        // Port 0 requests that the OS assigns us an available port.
+        let addr = std::net::TcpListener::bind("0.0.0.0:0")
+            .unwrap()
+            .local_addr()
+            .unwrap();
+
+        // Move the server into the background so that it's not blocking.
+        tokio::spawn(async move { server(addr, rx).await });
+
+        let res = reqwest::Client::new()
+            .get(format!("http://localhost:{}/api/v1/health", addr.port()))
+            .send()
+            .await
+            .unwrap();
+
+        tx.send(()).unwrap();
+
+        assert_eq!(res.status(), StatusCode::OK);
+        assert!(res.text().await.unwrap().is_empty());
+    }
 }
